e036844e35f01988cc2ad4393697c9371d0a24d2
Prashanthi Chirunomula <chirunomulap@gmail.com>
2020-11-22 12:03:57 -0600

    Area of Rectangle

---
+++ src/org/launchcode/java/demos/AreaOfRectangle.java
@@ -0,0 +1,16 @@
+package org.launchcode.java.demos;
+
+import java.util.Scanner;
+
+public class AreaOfRectangle {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the length of the rectangle: ");
+        float length = input.nextFloat();
+        System.out.println("Enter the width of the rectangle: ");
+        float width = input.nextFloat();
+        float area = length * width;
+        System.out.println(area);
+
+    }
+}
